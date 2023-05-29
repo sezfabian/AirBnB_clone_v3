7e416d51ecd0805f0ae63a4bf314b6738e0648fb
sezfabian <cheruiyotfabian@gmail.com>
2023-05-29 23:45:11 +0300

    pycodestyle9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -16,7 +16,8 @@ def get_users():
     return jsonify(users)
 
 
-@app_views.route('/users/<string:user_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """
     Retrieves a specific User object by its ID
@@ -27,7 +28,8 @@ def get_user(user_id):
     return jsonify(user.to_dict())
 
 
-@app_views.route('/users/<string:user_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id):
     """
     Deletes a specific User object by its ID
@@ -57,7 +59,8 @@ def create_user():
     return jsonify(user.to_dict()), 201
 
 
-@app_views.route('/users/<string:user_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/users/<string:user_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_user(user_id):
     """
     Updates a specific User object by its ID
